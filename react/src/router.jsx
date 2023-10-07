a235c319895319531eabe15034c56dbe721e7a81
Antonio Arjones Bello <lcs.arjones@gmail.com>
2023-10-07 20:48:31 +0100

    Componente listar usuarios pendiente de responsive y de ajustar el sidebar para ocultarlo

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,11 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "./views/login.jsx";
+import Home from "./views/Home/Home.jsx";
+import ListUsers from "./views/Users/ListUsers.jsx";
 
 const router = createBrowserRouter([
     {
         path: '/login',
         element: <Login />
-    }
+    },
+    {
+        path: '/',
+        element: <Home />,
+        children: [
+            {
+                path: '/Users',
+                element: <ListUsers />
+            }
+        ]
+    },
 ])
 
 export default router;
