7c4ee5d131adb9f075adff316ca58c5b85b1b3dd
Erik Viera Olivares <erik.viera@edosoft.es>
2023-10-09 10:50:21 +0100

    Fixes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'jwt-auth' => \App\Http\Middleware\JwtMiddleware::class,
-        'api-header' => \App\Http\Middleware\API::class
+        'api-header' => \App\Http\Middleware\Api::class
     ];
 }
