d1202324e038c4049c9b027323d3b6168376bc3d
Antonio Arjones Bello <lcs.arjones@gmail.com>
2023-10-10 21:49:00 +0100

    Se agrega la key de movil por mobile

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable implements JWTSubject
         'auth_token',
         'access_number',
         'role',
-        'movil',
+        'mobile',
         'active',
     ];
 
