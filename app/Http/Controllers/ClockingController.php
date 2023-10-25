6a5ecbbf3efaaf5318b7aa491f258ea596aa04bc
System Administrator <root@MacBook-Pro-de-Erik.local>
2023-10-25 20:22:10 +0100

    Home config

---
+++ app/Http/Controllers/ClockingController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Clocking;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+
+class ClockingController extends Controller
+{
+
+    public function getClocksActualDay() : JsonResponse
+    {
+        $user = Auth::user();
+        $currentDate = Carbon::now()->format('Y-m-d');
+        $clocks = Clocking::where('user_id', $user['id'])
+                        ->whereDate('date', '=', $currentDate)
+                        ->get();
+
+        return new JsonResponse([
+            'data' => $clocks,
+            'username' => $user['name']
+        ]);
+    }
+
+    public function store(Request $request) : JsonResponse
+    {       
+        $user = Auth::user();
+        $currentDate = Carbon::now()->setTimezone('Europe/London');
+        $type = $request->type;
+
+        $newClock = new Clocking();
+        $newClock->user_id = $user['id'];
+        $newClock->date = $currentDate;
+        $newClock->type = $type;
+        $newClock->save();
+
+        return new JsonResponse([
+            'success' => true,
+            'message' => 'Fichaje realizado'
+        ]);
+    }
+}
