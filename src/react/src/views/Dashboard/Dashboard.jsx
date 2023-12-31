09ecc27f754df73ce6dd6f8e39967e1b74db5d22
Erik Viera <erikv@ascendad.com>
2023-12-31 13:05:18 +0000

    Clocking types develop

--- src/react/src/views/Dashboard/Dashboard.jsx
+++ src/react/src/views/Dashboard/Dashboard.jsx
@@ -1,196 +1,221 @@
-import { useState, useEffect } from "react";
-import { Layout, Button, Card, Space, Typography, Row, Col, Modal } from "antd";
-import { ExclamationCircleFilled } from "@ant-design/icons";
-import { CalendarIcon, PlayIcon, PauseIcon } from "@heroicons/react/outline";
+import React, { useState, useEffect } from 'react';
+import { Layout, Button, Card, Space, Typography, Row, Col, Modal, Input, Radio } from 'antd';
+import { CalendarIcon, PlayIcon, PauseIcon } from '@heroicons/react/outline';
 import { useStateContext } from "../../contexts/ContextProvider";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 import axiosClient from "../../axios";
 
 const { Text } = Typography;
 const { Title } = Typography;
-const { confirm } = Modal;
 
 const App = () => {
-    const { userToken } = useStateContext();
-    const [userName, setUserName] = useState("");
-    const [clocks, setClocks] = useState([]);
-    const [worked, setWorked] = useState(0);
-    const [isWorking, setIsWorking] = useState(false);
-    const [count, setCount] = useState(0);
-    const navigate = useNavigate();
+  const { currentUser, userToken, setCurrentUser, setUserToken } = useStateContext();
+  const [userName, setUserName] = useState('');
+  const [clocks, setClocks] = useState([]);
+  const [worked, setWorked] = useState(0); 
+  const [isWorking, setIsWorking] = useState(false);
+  const [count, setCount] = useState(0);
+  const [newType, setNewType] = useState(1);
+  const [newComment, setNewComment] = useState('');
+  const [open, setOpen] = useState(false);
+  const [titleModal, setTitleModal] = useState('');
+  const navigate = useNavigate();
 
-    if (!userToken) {
-        navigate("/login");
+  if (!userToken) {
+    navigate('/login');
+  }
+
+  useEffect(() => {
+    if (userToken) {
+      getClocks();
+    }
+  }, []);
+  
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCount(count + 1);
+    }, 100);
+
+    if (clocks.length % 2 !== 0){
+      setIsWorking(true);
+      setTitleModal('Seleccione el motivo de la parada');
+    }else{
+      setIsWorking(false);
+      setTitleModal('¿Está seguro de realizar el fichaje de entrada?');
     }
 
-    useEffect(() => {
-        if (userToken) {
-            getClocks();
-        }
-    }, []);
+    let total = 0;
+    for (let i = 0; i < clocks.length; i += 2) {
+      const input = clocks[i] ? new Date(clocks[i].date) : null;
+      const output = clocks[i + 1] ? new Date(clocks[i + 1].date) : null;
 
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setCount(count + 1);
-        }, 100);
+      if (input && output) {
+        const auxCalc = output - input;
+        total += auxCalc;
+      }else if(input){
+        total += Date.now() - input;
+      }
+    }
 
-        if (clocks.length % 2 !== 0) {
-            setIsWorking(true);
-        } else {
-            setIsWorking(false);
-        }
+    const seconds = Math.floor(total / 1000);
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const secondsRestants = seconds % 60;
+    const formatedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(secondsRestants).padStart(2, '0')}`;
+    
+    setWorked(formatedTime);
+    return () => clearInterval(intervalId);
+  }, [count]);
 
-        let total = 0;
-        for (let i = 0; i < clocks.length; i += 2) {
-            const input = clocks[i] ? new Date(clocks[i].date) : null;
-            const output = clocks[i + 1] ? new Date(clocks[i + 1].date) : null;
-
-            if (input && output) {
-                const auxCalc = output - input;
-                total += auxCalc;
-            } else if (input) {
-                total += Date.now() - input;
-            }
-        }
-
-        const seconds = Math.floor(total / 1000);
-        const hours = Math.floor(seconds / 3600);
-        const minutes = Math.floor((seconds % 3600) / 60);
-        const secondsRestants = seconds % 60;
-
-        const formatedTime = `${String(hours).padStart(2, "0")}:${String(
-            minutes
-        ).padStart(2, "0")}:${String(secondsRestants).padStart(2, "0")}`;
-        setWorked(formatedTime);
-        return () => clearInterval(intervalId);
-    }, [count]);
-
-    const getClocks = () => {
-        axiosClient({
-            url: "/clocks/list",
-            method: "GET",
-        })
-            .then((response) => {
-                setClocks(response.data.data);
-                setUserName(response.data.username);
-            })
-            .catch(() => {
-                Modal.error({
-                    title: "Ha ocurrido un error inesperado",
-                    content:
-                        "Inténtalo más tarde o contacta con el administrador",
-                    okButtonProps: {
-                        style: { background: "green", color: "white" },
-                    },
-                });
-            });
-    };
-
-    const toggleWork = () => {
-        confirm({
-            title: "¿Está seguro de realizar el fichaje?",
-            icon: <ExclamationCircleFilled />,
-            content: "",
-            okText: "Sí",
-            cancelText: "Cancelar",
-            okButtonProps: {
-                style: { background: "green", color: "white" },
-            },
-            onOk() {
-                if (isWorking) {
-                    setIsWorking(false);
-                } else {
-                    setIsWorking(true);
-                }
-                saveClock(isWorking ? 2 : 1);
-            },
-            onCancel() {},
+  const getClocks = () => {
+    axiosClient({
+      url: "/clocks/list",
+      method: "GET"
+    })
+      .then((response) => {
+          setClocks(response.data.data);
+          setUserName(response.data.username);
+      })
+      .catch((error) => {
+        Modal.error({
+          title: 'Ha ocurrido un error inesperado',
+          content: 'Inténtalo más tarde o contacta con el administrador',
+          okButtonProps: {
+            style: { background: 'green', color: 'white' }
+          },
         });
-    };
+      });
+  }
 
-    const saveClock = (type) => {
-        axiosClient({
-            url: "/clocks/save",
-            method: "POST",
-            data: { type: type },
-        })
-            .then(() => {
-                getClocks();
-            })
-            .catch(() => {
-                Modal.error({
-                    title: "Ha ocurrido un error inesperado",
-                    content:
-                        "Inténtalo más tarde o contacta con el administrador",
-                    okButtonProps: {
-                        style: { background: "green", color: "white" },
-                    },
-                });
-            });
-    };
+  const onChange = (e) => {
+    setNewType(e.target.value);
+  };
 
-    return (
-        <Layout className="min-h-screen">
-            <Row>
-                <h1 className="pb-4 text-2xl">
-                    ¡Hola <b>{userName}</b>!
-                </h1>
-            </Row>
-            <Row justify="center" className="mt-4" gutter={24}>
-                <Col span={24}>
-                    <Card>
-                        <Space direction="vertical" style={{ width: "100%" }}>
-                            <Text className="inline-flex">
-                                <CalendarIcon
-                                    color="green"
-                                    className="w-5 h-5"
-                                />
-                                <p className="ml-2">
-                                    Tu trabajo durante el{" "}
-                                    {new Date().toLocaleDateString()}
-                                </p>
-                            </Text>
+  const onChangeComment = (e) => {
+    setNewComment(e.target.value);
+  };
 
-                            <Title
-                                level={2}
-                                className="flex justify-center items-center"
-                            >
-                                {worked != 0 ? worked : "00:00:00"}
-                            </Title>
-                        </Space>
-                    </Card>
-                </Col>
-            </Row>
-            <Row justify="center" className="mt-2" gutter={24}>
-                <Col
-                    span={24}
-                    className="flex justify-center align-items-center"
-                >
-                    <Button
-                        type="primary"
-                        onClick={toggleWork}
-                        style={
-                            isWorking
-                                ? { background: "red" }
-                                : { background: "green" }
-                        }
-                    >
-                        {isWorking ? (
-                            <p className="inline-flex">
-                                <PauseIcon className="w-5 h-5" />{" "}
-                                <p className="ml-2">Pausar jornada laboral</p>
-                            </p>
-                        ) : (
-                            <p className="inline-flex">
-                                <PlayIcon className="w-5 h-5" />{" "}
-                                <p className="ml-2">Empezar jornada laboral</p>
-                            </p>
-                        )}
-                    </Button>
-                </Col>
-            </Row>
-        </Layout>
-    );
+  const handleOkStart = () => {
+    setOpen(false);
+    setIsWorking(true);
+    saveClock(1, '');
+  };
+
+  const handleOkStop = () => {
+    setOpen(false);
+    setIsWorking(false);
+    saveClock(newType, newComment);
+    setNewType(1);
+    setNewComment('');
+  };
+
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
+  const showModal = () => {
+    setOpen(true);
+  };
+
+  const toggleWork = () => {
+    showModal();
+  }
+
+  const saveClock = (type_id, comments) => {
+    axiosClient({
+      url: "/clocks/save",
+      method: "POST",
+      data: {'type_id': type_id, 'comments': comments}
+    })
+      .then((response) => {
+        getClocks();
+      })
+      .catch((error) => {
+        Modal.error({
+          title: 'Ha ocurrido un error inesperado',
+          content: 'Inténtalo más tarde o contacta con el administrador',
+          okButtonProps: {
+            style: { background: 'green', color: 'white' }
+          },
+        });
+      });
+  }
+
+  return (
+    <Layout className='min-h-screen'>
+      <Row>
+        <h1 className="pb-4 text-2xl">¡Hola <b>{userName}</b>!</h1>
+      </Row>
+      <Row justify="center" className='mt-4' gutter={24}>
+        <Col span={24}>
+          <Card>
+            <Space direction="vertical" style={{ width: "100%" }}>
+              <Text className='inline-flex'> 
+                <CalendarIcon color='green' className="w-5 h-5"/> 
+                <p className='ml-2'>Tu trabajo durante el {new Date().toLocaleDateString()}</p>
+              </Text>
+
+              <Title level={2} className='flex justify-center items-center'>
+                {worked != 0 ? worked : '00:00:00'}
+              </Title>
+            </Space>
+          </Card>
+        </Col>
+      </Row>
+      <Row justify="center" className='mt-2' gutter={24}>
+        <Col span={24} className='flex justify-center align-items-center'>
+          <Button
+              type='primary'
+              onClick={toggleWork}
+              style={isWorking ? { background: "red" } : {background: "green"}}>
+              {isWorking ? 
+              (<p className='inline-flex'><PauseIcon className="w-5 h-5"/> <p className='ml-2'>Pausar jornada laboral</p></p>) 
+              : (<p className="inline-flex"><PlayIcon className="w-5 h-5"/> <p className='ml-2'>Empezar jornada laboral</p></p>)
+              }
+          </Button>
+        </Col>
+      </Row>
+
+      <Modal
+        open={open}
+        title={titleModal}
+        onCancel={handleCancel}
+        footer={[
+          <div className='mt-4'>
+          <Button key="back" onClick={handleCancel}>
+            Cancelar
+          </Button>
+          <Button hidden={isWorking} key="start" 
+                  style={{ background: 'green', color: 'white' }}
+                  onClick={handleOkStart}>
+           Fichar entrada
+         </Button>
+          <Button hidden={!isWorking} key="stop" 
+                  disabled={newType == 1} style={{ background: newType == 1 ? 'grey' : 'green', color: 'white' }}
+                  onClick={handleOkStop}
+                  >
+            Fichar
+          </Button>
+          </div>
+        ]}
+      >
+        <div hidden = {!isWorking}>
+        <Radio.Group onChange={onChange} value={newType}>
+            <Space direction="vertical">
+              <Radio value={2}>Finalizar la jornada</Radio>
+              <Radio value={3}>Comida</Radio>
+              <Radio value={4}>Médico</Radio>
+              <Radio value={5}>Permiso</Radio>
+              <Radio value={6}>
+                Otro...
+                {newType === 6 ? <Input value={newComment} onChange={onChangeComment} style={{ width: 100, marginLeft: 10 }} /> : null}
+              </Radio>
+            </Space>
+          </Radio.Group>
+          </div>
+      </Modal>
+    </Layout>
+  );
 };
 
 export default App;
--- src/react/src/views/MyClocks/MyClocks.jsx
+++ src/react/src/views/MyClocks/MyClocks.jsx
@@ -1,153 +1,139 @@
-import { useState, useEffect } from "react";
-import {
-    ConfigProvider,
-    Layout,
-    Row,
-    Table,
-    Col,
-    DatePicker,
-    Button,
-    Modal,
-} from "antd";
-import es_ES from "antd/es/locale/es_ES";
-import { ArrowSmRightIcon, ArrowSmLeftIcon } from "@heroicons/react/outline";
+import React, { useState, useEffect } from 'react';
+import { ConfigProvider, Layout, Row, Table, Col, DatePicker, Button, Modal, Tooltip } from 'antd';
+import es_ES from 'antd/es/locale/es_ES';
+import { ArrowSmRightIcon, ArrowSmLeftIcon } from '@heroicons/react/outline';
 import { useStateContext } from "../../contexts/ContextProvider";
+import { useNavigate } from 'react-router-dom';
 import axiosClient from "../../axios";
-import moment from "moment";
-import "moment/locale/es";
-import dayjs from "dayjs";
-import "dayjs/locale/es";
+import moment from 'moment';
+import 'moment/locale/es';
+import dayjs from 'dayjs';
+import 'dayjs/locale/es';
 
-moment.locale("es");
-dayjs.locale("es");
+moment.locale('es');
+dayjs.locale('es');
 
 const { RangePicker } = DatePicker;
-const dateFormat = "DD/MM/YYYY";
+const dateFormat = 'DD/MM/YYYY';
 
 const MyClocks = () => {
-    const { userToken } = useStateContext();
-    const [clocks, setClocks] = useState([]);
-    const [startDate, setStartDate] = useState(null);
-    const [endDate, setEndDate] = useState(null);
+  const { currentUser, userToken, setCurrentUser, setUserToken } = useStateContext();
+  const [user, setUser] = useState('');
+  const [clocks, setClocks] = useState([]);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (userToken) {
-            getClocks();
-        }
-    }, []);
+  useEffect(() => {
+    if (userToken) {
+      getClocks();
+    }
+  }, []);
 
-    const getClocks = () => {
-        axiosClient({
-            url: "/clocks/all",
-            method: "POST",
-            data: { startDate: startDate, endDate: endDate },
-        })
-            .then((response) => {
-                setClocks(response.data.data);
-            })
-            .catch(() => {
-                Modal.error({
-                    title: "Ha ocurrido un error inesperado",
-                    content:
-                        "Inténtalo más tarde o contacta con el administrador",
-                    okButtonProps: {
-                        style: { background: "green", color: "white" },
-                    },
-                });
-            });
-    };
+  const getClocks = () => {
+    axiosClient({
+      url: "/clocks/all",
+      method: "POST",
+      data: {'startDate': startDate, 'endDate': endDate}
+    })
+      .then((response) => {
+          setClocks(response.data.data);
+          setUser(response.data.user);
+      })
+      .catch((error) => {
+        Modal.error({
+          title: 'Ha ocurrido un error inesperado',
+          content: 'Inténtalo más tarde o contacta con el administrador',
+          okButtonProps: {
+            style: { background: 'green', color: 'white' }
+          },
+        });
+      });
+  }
 
-    const handleDateChange = (dates, dateString) => {
-        if (dateString.length == 2) {
-            setStartDate(dateString[0]);
-            setEndDate(dateString[1]);
-        } else {
-            setStartDate(null);
-            setEndDate(null);
-        }
-    };
+  const handleDateChange = (dates, dateString) => {
+   if(dateString.length == 2)
+   {
+    setStartDate(dateString[0]);
+    setEndDate(dateString[1]);
+   }else{
+    setStartDate(null);
+    setEndDate(null);
+   }
+  };
 
-    const handleSearch = () => {
-        getClocks();
-    };
+  const handleSearch = () => {
+    getClocks();
+  };
 
-    const columns = [
-        {
-            title: "Día",
-            dataIndex: "day",
-            render: (day) => <p>{day}</p>,
-        },
-        {
-            title: "Fichajes",
-            dataIndex: "dates",
-            render: (dates) => (
-                <div>
-                    {dates.map((date, index) => (
-                        <div key={index}>
-                            <p className="inline-flex">
-                                {date.type == 1 ? (
-                                    <ArrowSmRightIcon
-                                        className="w-5 h-5"
-                                        style={{ color: "green" }}
-                                    />
-                                ) : (
-                                    <ArrowSmLeftIcon
-                                        className="w-5 h-5"
-                                        style={{ color: "red" }}
-                                    />
-                                )}
+  const columns = [
+    {
+        title: "Día",
+        dataIndex: "day",
+        render: (day) => (
+          <p>{day}</p>
+        ),
+    },
+    {
+        title: "Fichajes",
+        dataIndex: "dates",
+        render: (dates) => (
+          <div>
+            {dates.map((date, index) => (
+                <div key={index}>
+                    <p className='inline-flex'>
+                      {date.type == 1 ? 
+                      <ArrowSmRightIcon className="w-5 h-5" style={ {color: 'green'} }/> : 
+                      <ArrowSmLeftIcon className="w-5 h-5" style={ {color: 'red'} } />} 
 
-                                {date.type == 1 ? "Entrada " : "Salida "}
+                      <Tooltip placement="top" title={date.comments}>
+                        {date.type_name} <>&nbsp;</>
+                      </Tooltip>
 
-                                {moment(date.day).format("HH:mm:ss")}
-                            </p>
-                        </div>
-                    ))}
+                      {moment(date.day).format('HH:mm:ss')}
+                    </p>
                 </div>
-            ),
-        },
-        {
-            title: "Tiempo trabajado",
-            dataIndex: "total",
-            render: (total) => <p>{total} horas</p>,
-        },
-    ];
+            ))}
+          </div>
+        ),
+    },
+    {
+        title: "Tiempo trabajado",
+        dataIndex: "total",
+        render: (total) => (
+          <p>{total} horas</p>
+      ),
+    }
+  ];
 
-    return (
-        <ConfigProvider locale={es_ES}>
-            <Layout className="min-h-screen">
-                <Row>
-                    <h1 className="pb-4 text-2xl">Mis fichajes</h1>
-                </Row>
-                <Row justify="left" className="mt-4" gutter={24}>
-                    <Col span={24} className="inline-flex">
-                        <RangePicker
-                            format={dateFormat}
-                            onChange={handleDateChange}
-                        />
-                        <Button
-                            className="button-antd-custom ml-2"
-                            type="primary"
-                            onClick={handleSearch}
-                        >
-                            Buscar
-                        </Button>
-                    </Col>
-                </Row>
-                <Row justify="center" className="mt-4" gutter={24}>
-                    <Col span={24}>
-                        <Table
-                            pagination={{ pageSize: 7 }}
-                            columns={columns}
-                            dataSource={clocks}
-                            scroll={{ x: 500 }}
-                            rowKey="id"
-                        />
-                    </Col>
-                </Row>
-            </Layout>
-        </ConfigProvider>
-    );
+  return (
+    <ConfigProvider locale={es_ES}>
+    <Layout className='min-h-screen'>
+        <Row>
+          <h1 className="pb-4 text-2xl">Mis fichajes</h1>
+        </Row>
+        <Row justify="left" className='mt-4' gutter={24}>
+          <Col span={24} className='inline-flex'>
+            <RangePicker format={dateFormat} onChange={handleDateChange} />
+            <Button className="button-antd-custom ml-2"
+                    type="primary"
+                    onClick={handleSearch}>
+              Buscar
+            </Button>
+          </Col>
+        </Row>
+        <Row justify="center" className='mt-4' gutter={24}>
+          <Col span={24}>
+            <Table pagination={{ pageSize: 7}} 
+                   columns={columns} 
+                   dataSource={clocks} 
+                   scroll={{ x: 500 }}
+                   rowKey="id" />
+          </Col>
+        </Row>
+    </Layout>
+    </ConfigProvider>
+  );
 };
 
 export default MyClocks;
